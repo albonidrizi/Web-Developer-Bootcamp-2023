7ff2aa1806c4338571bf86b6ecdbecb2dd698a73
Albon Idrizi <albonidrizi@gmail.com>
2023-07-18 12:10:34 +0200

    wather app

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,29 +1,48 @@
-const { log } = require("console");
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", (req, res)=>{
+ res.sendFile(__dirname + "/index.html")
 
-    const url ="https://api.openweathermap.org/data/2.5/weather?q=tetovo,mk&appid=af9fd2dec475580436d70079d39ba065";
-
-    https.get(url, (res)=>{
-        console.log(res.statusCode);
-
-        res.on("data", (data)=>{
-            console.log(data);
-        })
-    });
-
-   res.send("Server is up and running.");
 })
 
+app.post("/", (req,res)=>{
 
+  const query = req.body.cityName;
+  const unite = "matric";
+  const apiKey = "af9fd2dec475580436d70079d39ba065";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" + query +"&appid=" +apiKey +"&units" + unite;
 
+  https.get(url, (response) => {
+    console.log(response.statusCode);
 
+    response.on("data", (data) => {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
 
+      const name = weatherData.name;
+      const icon = weatherData.weather[0].icon;
+      const imgUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+      const celcius = temp - 273.15;
+
+      res.write("<h1>In the city of " + name + "</h1>");
+      res.write("<h2>The wather is cuttently " + description + "</h2>");
+      res.write(
+        "<h3>The temperature is " + celcius + " degrees Celcius.</h3>"
+      );
+      res.write("<img src=" + imgUrl + ">");
+      res.send();
+    });
+  });
+})
 
 
 app.listen(3000, ()=>{
--- my-express-server/server.js
+++ my-express-server/server.js
@@ -9,3 +9,16 @@
  app.listen(port, () => {
    console.log(`Example app listening on port ${port}`);
  });
+
+ app.get("/contact", (req, res)=>{
+
+  res.send("Contact me at : albonidrizi@gmail.com ");
+ });
+
+ app.get("/about", (req,res)=>{
+  res.send("I am software developer with 3 years experience, and I like to code")
+ })
+
+ app.get("/hobbies",(req,res)=>{
+  res.send("<ul><li>coffee</li><li>code</li><li>sport</li></ul>");
+ })
